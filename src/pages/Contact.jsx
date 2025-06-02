5c38f4d342b4665d016ec5e190841cd89acc5090
mugilanm23 <mugilanm23112005@gmail.com>
2025-06-02 22:29:13 +0530

    intial commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -36,7 +36,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try{
-      const response = await fetch('https://www.digitaltouchcorp.com/api/contact', {
+      const response = await fetch('https://api.digitaltouchcorp.com/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData)
@@ -65,7 +65,7 @@ const Contact = () => {
   const handleScheduleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://www.digitaltouchcorp.com/api/schedule', {
+      const response = await fetch('https://api.digitaltouchcorp.com//api/schedule', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(scheduleData)
